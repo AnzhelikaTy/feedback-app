cb412ab986f35b2163279ffc0456a8607016c6d8
Anzhelika Susemcik <angelika.tyrnova@gmail.com>
2022-12-20 17:54:30 -0500

    added PropTypes

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,5 @@
 import FeedbackItem from "./FeedbackItem";
+import PropTypes from "prop-types";
 
 import React from "react";
 
@@ -14,5 +15,15 @@ function FeedbackList({ feedback }) {
     </div>
   );
 }
+
+FeedbackList.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    })
+  ),
+};
 
 export default FeedbackList;
